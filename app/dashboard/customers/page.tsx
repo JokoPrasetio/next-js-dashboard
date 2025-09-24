9a7adc52507f4bb1a7ffe345538a74247488f262
JokoPrasetio <joko.prasetio@rsabhk.co.id>
2025-09-24 14:42:02 +0700

    add page customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { lusitana } from "@/app/ui/font";
+import Search from "@/app/ui/search";
+import { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Page(props: {
+  searchParams?:Promise<{
+    query?: string;
+  }>
+}) {
+  const searchParams = await props.searchParams;
+  const data  = await fetchFilteredCustomers(searchParams?.query || '');
+  return (
+    <div className="w-full">
+        <Table customers={data} />
+    </div>
+  );
 }
