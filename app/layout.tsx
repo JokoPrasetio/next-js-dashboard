d797ce60e3c5600e4d9291c9d1a5a791ea4c10ae
JokoPrasetio <joko.prasetio@rsabhk.co.id>
2025-09-24 11:10:37 +0700

    course next js done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/font';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Jooooo Dashboard',
+  description: 'The official next js course dashboard, buith with app router',
+  metadataBase: new URL('https://rumahjooocode.com')
+}
 export default function RootLayout({
   children,
 }: {
