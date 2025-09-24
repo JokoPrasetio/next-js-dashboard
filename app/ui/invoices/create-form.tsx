f91e27e600f37edc488a7a3073a293adf72d4fb0
JokoPrasetio <joko.prasetio@rsabhk.co.id>
2025-09-24 11:32:19 +0700

    course next js done 3

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -43,8 +43,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId && state.errors.customerId.map((error: string) => (
-              <p className='mt-2 text-sm text-red-500'>
+            {state.errors?.customerId && state.errors.customerId.map((error: string, index: number) => (
+              <p key={index} className='mt-2 text-sm text-red-500'>
                 {error}
               </p>
             ))}
@@ -119,8 +119,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
             <div id="status-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.status && state.errors.status.map((error: string) => (
-                <p className='mt-2 text-sm text-red-500'>
+              {state.errors?.status && state.errors.status.map((error: string, index: number) => (
+                <p key={index} className='mt-2 text-sm text-red-500'>
                   {error}
                 </p>
               ))}
